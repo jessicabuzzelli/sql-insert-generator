c2b8bf3ef0ecffd11311bf1da2e0b7519526056b
Jessica Buzzelli <47726683+jessicabuzzelli@users.noreply.github.com>
2021-02-12 23:30:53 -0500

    delete examples

--- examples/test_csv.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE tablename (col1 VARCHAR(2), col2 NUMBER(4,0), col3 NUMBER(22,2)); 
-INSERT INTO tablename (col1, col2, col3) VALUES (1, 2, 3);
-INSERT INTO tablename (col1, col2, col3) VALUES (4, 5, 6);
-INSERT INTO tablename (col1, col2, col3) VALUES (7, 8, 9);
--- examples/test_excel.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE tablename (col1 VARCHAR(2), col2 NUMBER(4,0), col3 NUMBER(22,2)); 
-INSERT INTO tablename (col1, col2, col3) VALUES (1.0, 2.0, 3.0);
-INSERT INTO tablename (col1, col2, col3) VALUES (four, five, six);
-INSERT INTO tablename (col1, col2, col3) VALUES (7.1, 8.22, 8.333);
