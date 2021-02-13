3b0076495c290948f3f78790f82b64c8732554a3
jessicabuzzelli <jessicambuzzelli@gmail.com>
2021-02-12 23:51:06 -0500

    rehome examples

---
+++ examples/test_csv.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test_csv (col1 NUMBER(22,0), col2 NUMBER(22,0), col3 NUMBER(22,0)); 
+INSERT INTO test_csv (col1, col2, col3) VALUES (1, 2, 3);
+INSERT INTO test_csv (col1, col2, col3) VALUES (4, 5, 6);
+INSERT INTO test_csv (col1, col2, col3) VALUES (7, 8, 9);
